c57dc723a6aa9bac84b5eada2c9f69d53d918658
dnyuhm <dylanmeuriot@gmail.com>
2022-02-10 11:53:43 +0100

    Commit 25 & 26 : Projection d’un tableau dynamique

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/core/models/order';
 import { OrdersService } from '../../services/orders.service';
 
 @Component({
@@ -8,11 +9,24 @@ import { OrdersService } from '../../services/orders.service';
 })
 export class PageListOrdersComponent implements OnInit {
   public title: string;
+  public collection!: Order[];
+  public headers: string[];
 
   constructor(private ordersService: OrdersService) {
     this.title = 'list of orders';
+    this.headers = [
+      'type',
+      'client',
+      'nbJours',
+      'tjmHt',
+      'totol HT',
+      'total TTC',
+      'state',
+    ];
 
-    this.ordersService.collection$.subscribe((data) => console.log(data));
+    this.ordersService.collection$.subscribe(
+      (data) => (this.collection = data)
+    );
   }
 
   ngOnInit(): void {}
---
+++ src/app/shared/components/table-light/table-light.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-light',
+  templateUrl: './table-light.component.html',
+  styleUrls: ['./table-light.component.scss'],
+})
+export class TableLightComponent implements OnInit {
+  @Input() public headers!: string[];
+
+  constructor() {
+    console.log(this.headers);
+  }
+
+  ngOnChanges(): void {
+    console.log(this.headers);
+  }
+
+  ngOnInit(): void {
+    console.log(this.headers);
+  }
+}
